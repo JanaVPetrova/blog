ffe7cbcb1b789350b40de6d4accc90afe7c83fab
JanaVPetrova <janavpetrova@gmail.com>
2013-07-17 16:04:48 +0200

    remove TSA-style tests in models/comment and models/post

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,8 +1,4 @@
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
-  test "should not save comment without commenter" do
-    comment = Comment.new
-    assert !comment.save
-  end
 end
