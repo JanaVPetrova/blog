95f659cbe1116efc40d2120c93b08a00723ca34d
JanaVPetrova <janavpetrova@gmail.com>
2013-08-02 11:39:31 +0200

    replace collection_select with custom state_event_input

---
+++ app/inputs/state_event_input.rb
@@ -0,0 +1,32 @@
+class StateEventInput < SimpleForm::Inputs::FileInput
+  def collection
+    object.send(transitions_method_name)
+  end
+
+  def input
+    label_method = :human_to_name
+    value_method = :event
+    input_options = { include_blank: object.send(human_state_name_method) }
+
+    out = @builder.collection_select(
+      attribute_name, collection, value_method, label_method,
+      input_options, input_html_options
+    )
+    out.html_safe
+  end
+
+  private
+  def machine_name
+    machine_name = attribute_name.to_s.split("_")
+    machine_name.delete "event"
+    machine_name.join("_")
+  end
+
+  def transitions_method_name
+    "#{machine_name}_transitions"
+  end
+
+  def human_state_name_method
+    "human_#{machine_name}_name"
+  end
+end
