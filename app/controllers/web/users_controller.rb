6253021721b9a2d1f7e31876206bd63dd6004f0d
JanaVPetrova <janavpetrova@gmail.com>
2013-08-27 12:47:06 +0200

    use UserEditType

--- app/controllers/web/users_controller.rb
+++ app/controllers/web/users_controller.rb
@@ -10,7 +10,7 @@ class Web::UsersController < Web::ApplicationController
   end
 
   def create
-    @user = User.new user_params
+    @user = UserEditType.new params[:user]
 
     if @user.save
       redirect_to posts_path
@@ -31,10 +31,16 @@ class Web::UsersController < Web::ApplicationController
 
   def update
     @user = User.find params[:id]
+    if current_user == @user
+      @user = @user.becomes UserEditType
 
-    if @user.update_attributes user_params
-      redirect_to user_path(@user)
+      if @user.update_attributes params[:user]
+        redirect_to user_path(@user)
+      else
+        redirect_to edit_user_path(@user)
+      end
     else
+      f(:error)
       redirect_to edit_user_path(@user)
     end
   end
--- test/factories/users.rb
+++ test/factories/users.rb
@@ -2,10 +2,12 @@ FactoryGirl.define do
   factory :user do
     login { generate :string }
     password { generate :string }
+    email { generate :email }
   end
 
   factory :owner, parent: :user do
     login { configus.owner.login }
     password { configus.owner.password }
+    email { configus.owner.email }
   end
 end
