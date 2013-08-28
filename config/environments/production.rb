feaae7c4689d86067842fd40fc8cf36ff14c9521
JanaVPetrova <janavpetrova@gmail.com>
2013-08-28 10:00:26 +0200

    mail settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,13 @@ Blog::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'baci.lindsaar.net',
+    user_name:            'railsblog2013',
+    password:             'JDpqFS8MMht6kgsVxYqR7YEtD',
+    authentication:       'plain',
+    enable_starttls_auto: true  }
 end
