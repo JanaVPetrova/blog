789f3540868449bdd69009dc732fa91c5a07bd96
Jana Petrova <janavpetrova@gmail.com>
2014-01-11 18:04:29 +0000

    remove image from posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,4 @@
 class Post < ActiveRecord::Base
-  mount_uploader :image, ImageUploader
-
   validates :title, presence: true,
                     length: { minimum: 5 }
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130827100836) do
+ActiveRecord::Schema.define(version: 20140111175552) do
 
   create_table "post_comments", force: true do |t|
     t.text     "body"
@@ -32,7 +32,6 @@ ActiveRecord::Schema.define(version: 20130827100836) do
     t.datetime "updated_at"
     t.string   "state"
     t.string   "validation_state"
-    t.string   "image"
   end
 
   create_table "users", force: true do |t|
